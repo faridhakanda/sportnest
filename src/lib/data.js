d3528761bffaa5ac583d2b5cb8d6c22c883383ba
faridhakanda <faridakanda1729@gmail.com>
2026-05-21 17:11:29 +0600

    update facility detail page

--- src/lib/data.js
+++ src/lib/data.js
@@ -7,16 +7,16 @@ export const getAllSportFacilities = async() => {
     return data;
 }
 export const getFacilityDetailsById = async(id) => {
-    const { token } = await auth.api.getToken({
-        headers: await headers()
-    })
+    // const { token } = await auth.api.getToken({
+    //     headers: await headers()
+    // })
     const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/facilities/${id}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            authorization: `Bearer ${token}`
+            //authorization: `Bearer ${token}`
         },
-        cache: 'no-store'
+        //cache: 'no-store'
     });
     const data = await res.json();
     return data;
