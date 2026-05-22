c9c4b74ea2e7f020f6d4a9987c3ba3415613b38f
faridhakanda <faridakanda1729@gmail.com>
2026-05-22 17:56:30 +0600

    now error and not-found page is update

--- src/app/error.js
+++ src/app/error.js
@@ -1,12 +1,26 @@
-'use client';
-import React from 'react';
+"use client";
+import { Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h2>Error Page!</h2>
-        </div>
-    );
+  return (
+    <div className="grid mx-auto my-auto items-center justify-center">
+      <Image
+      className="mx-2"
+        src="/error404.webp"
+        alt="error404 page"
+        width={400}
+        height={400}
+      />
+      <Link className="mx-auto justify-center my-2" href={"/"}>
+        <Button className={"rounded-md font-bold mx-auto justify-center my-2"}>
+          Go to Home
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const NotFound404 = () => {
-    return (
-        <div>
-            <h2>Not found: 404!</h2>
-        </div>
-    );
+  return (
+    <div className="grid mx-auto my-auto items-center justify-center">
+      <Image
+      className="mx-2"
+        src="/error404.webp"
+        alt="error404 page"
+        width={400}
+        height={400}
+      />
+      <Link className="mx-auto justify-center my-2" href={"/"}>
+        <Button className={"rounded-md font-bold mx-auto justify-center my-2"}>
+          Go to Home
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound404;
